428d8a2f71e8af4cf08205da27e130469e620e02
arda6 <74415046+arda6@users.noreply.github.com>
2020-11-17 23:32:24 +0300

    Add files via upload

--- agizleyici.py
+++ agizleyici.py
@@ -16,5 +16,5 @@ os.system("figlet Ağ Tarayıcısı Başlatılıyor. ")
 arp1 = scapy.ARP(pdst="10.0.2.1/24")
 broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 toplu = arp1/broadcast
-(answered_list,unanswered_list) = scapy.srp(toplu,timeuot=1)
+(answered_list,unanswered_list) = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")(/scapy.srp(toplu,timeuot=1)
 answered_list.summary()
